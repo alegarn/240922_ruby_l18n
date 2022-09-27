2c4a3cf3b037f608edede571e3aa2058715f7dcc
alegarn <alexgarnier78310@protonmail.com>
2022-09-27 15:43:25 +0200

    gem http_accept_language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
 	before_action :set_locale
 
-	private
+  private
 
-	def set_locale
-		if params[:locale]
-		I18n.locale = params[:locale]
-		end
-	end
+  def set_locale
+    if !params[:locale]
+      I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales)
+    else
+      I18n.locale = params[:locale]
+    end
+  end
 end
